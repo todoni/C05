9039a6f7e91f3a47412c9c0ffe27f6305cffa42d
todoni <hansojeong95@gmail.com>
2021-04-06 13:17:55 +0900

    Done up to ex03

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: sohan <sohan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 04:43:53 by sohan             #+#    #+#             */
-/*   Updated: 2021/04/04 06:17:38 by sohan            ###   ########.fr       */
+/*   Updated: 2021/04/06 13:16:21 by sohan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,29 @@
 int	ft_fibonacci(int index)
 {
 	//int fibo0;
+	static int fibo2 = 0;
 	static int fibo1 = 1;
-	static int fibo2 = 1;
 	static int fibo0 = 0;
+	int check;
 	
-	printf("%d ", fibo2);
+	check = 0;
 	if (index == 0)
-		return (fibo2 + fibo1);
+		return (0);
+	else if (index == 1)
+		return (1);
+	if (index == 2)
+		return (fibo2);
 	//fibo1 = fibo2;
+	fibo2 = fibo1 + fibo0;
 	fibo0 = fibo1;
-	fibo1 = fibo2;	
-	fibo2 = fibo2 + fibo1;
+	fibo1 = fibo2;
+	check = 1;
 	return (ft_fibonacci(index - 1));
 
 }
 
 int main()
 {
-	printf("%d\n", ft_fibonacci(10));
+	printf("%d\n", ft_fibonacci(3));
 	return  0;
 }
